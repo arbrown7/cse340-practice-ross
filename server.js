b058fac2d19f40f652b6263cc3d1c9427d6f2aef
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-09 22:39:37 -0700

    unit 1 readings

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+const name = process.env.NAME; // <-- NEW
+
+app.get('/', (req, res) => {
+    res.send(`Hello, ${name}!`); // <-- UPDATED
+});
+
+app.get('/new-route', (req, res) => {
+    res.send('This is a new route!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('This is the hello page');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
