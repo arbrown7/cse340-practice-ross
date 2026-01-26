1d64d1e30d0c3bbb2f782ff5234386524e28f65b
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-25 19:10:16 -0700

    Introduction to Middleware
    Understanding and Using Middleware in Express

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.set('view engine', 'ejs');
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
+app.use((req, res, next) => {
+    // Skip logging for routes that start with /. (like /.well-known/)
+    if (!req.path.startsWith('/.')) {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
 /**
  * Global template variables middleware
  * 
