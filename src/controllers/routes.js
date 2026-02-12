27d227f619e4bbc5e21f3c7b9ca7e061fa71071a
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-12 09:56:08 -0700

    5. Create the Contact Controller
    Building Your First Form: Contact Us

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { addDemoHeaders } from '../middleware/demo/headers.js';
 import { catalogPage, courseDetailPage } from './catalog/catalog.js';
 import { facultyListPage, facultyDetailPage } from './faculty/faculty.js';
+import contactRoutes from './forms/contact.js';
 import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
 
 // Create a new router instance
@@ -36,6 +37,9 @@ router.get('/catalog/:slugId', courseDetailPage);
 // Faculty routes
 router.get('/faculty', facultyListPage);
 router.get('/faculty/:slugId', facultyDetailPage);
+
+// Contact form routes
+router.use('/contact', contactRoutes);
 
 // Demo page with special middleware
 router.get('/demo', addDemoHeaders, demoPage);
